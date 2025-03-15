640958b3a8142c9a49feb279e38b5e548a022ab7
ArkodevMukherjee <78291548+ArkodevMukherjee@users.noreply.github.com>
2025-03-15 17:51:16 +0530

    Add files via upload

---
+++ chatbots.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from langchain_huggingface import HuggingFaceEndpoint,ChatHuggingFace
+from langchain_core.messages import SystemMessage,AIMessage,HumanMessage
+from langchain_core.prompts import PromptTemplate
+
+
+st.title("Your personal chatbot is here")
+
+model = HuggingFaceEndpoint(
+    repo_id="Qwen/QwQ-32B",
+    task="text-generation"
+)
+
+if "messages" not in st.session_state:
+    st.session_state.messages = [
+        SystemMessage(content="Hello! I am your personal chatbot. How can I help you today?"),
+        AIMessage(content="I am a chatbot.")
+    ]
+
+# Display chat history
+for msg in st.session_state.messages:
+    role = "**You:**" if isinstance(msg, HumanMessage) else "**Bot:**"
+    st.write(role, msg.content)
+
+
+text = st.text_input("Enter what you want to query")
+button = st.button("Send")
+
+
+if button and text != "":
+    st.session_state.messages.append(HumanMessage(content=text))
+    text =""
+    botMessages = model.invoke(st.session_state.messages).split("AI:")[-1].strip()
+    st.session_state.messages.append(AIMessage(content=botMessages))
+    st.rerun()
